e27b5fec2280b7bfb85369073f874145330cf01b
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-16 03:36:53 +0600

    added delete functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -41,6 +41,14 @@ async function run() {
       console.log(newCoffee);
 
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    // delete
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     });
 
