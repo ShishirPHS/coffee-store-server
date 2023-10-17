791512db88457364aaa485f2374fec2e2de636ea
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-17 20:03:59 +0600

    get users infos from database

--- index.js
+++ index.js
@@ -85,6 +85,12 @@ async function run() {
     });
 
     // user related APIs
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/user", async (req, res) => {
       const user = req.body;
       console.log(user);
